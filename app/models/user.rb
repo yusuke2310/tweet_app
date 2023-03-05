b37a0c7a70543260d724ff34780558d2a042f746
Ubuntu <ubuntu@ip-172-31-10-61.ap-northeast-1.compute.internal>
2023-03-05 01:31:33 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    def has_secure_password
+    end
+    
+    validates :name, {presence: true }
+    validates :email, {presence: true , uniqueness:true }
+    
+    def posts
+        return Post.where(user_id: self.id)
+    end
+end
